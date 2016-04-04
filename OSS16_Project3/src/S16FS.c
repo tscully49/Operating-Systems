bf3e26fa27fae96874227bf1988d0271cc31db8d
Thomas Scully <thomas1374@yahoo.com>
2016-04-04 13:30:48 -0500

    Working on makefile

--- OSS16_Project3/src/S16FS.c
+++ OSS16_Project3/src/S16FS.c
@@ -4,3 +4,26 @@
 
 // directory, directory block, file descriptor tables ---> From design 
 
+S16FS_t *fs_format(const char *path) {
+	if (strcmp(path, "") == 0 || strcmp(path, "\n") == 0 || !path) return NULL;
+
+	return NULL;
+}
+
+S16FS_t *fs_mount(const char *path) {
+	if (strcmp(path, "") == 0 || strcmp(path, "\n") == 0 || !path) return NULL;
+
+	return NULL;
+}
+
+int fs_unmount(S16FS_t *fs) {
+	if (!fs) return -1;
+
+	return -1;
+}
+
+int fs_create(S16FS_t *fs, const char *path, file_t type) {
+	if (!fs || strcmp(path, "") == 0 || strcmp(path, "\n") == 0 || !path || !type) return -1;
+
+	return -1;
+}
