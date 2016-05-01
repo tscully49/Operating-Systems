a6270f08fe8b5ab92a0977d7855510365856155f
Thomas Scully <thomas1374@yahoo.com>
2016-04-30 20:14:13 -0500

    Done, just need to add comments

--- OSS16_Assignment3/src/producer_consumer.c
+++ OSS16_Assignment3/src/producer_consumer.c
@@ -24,6 +24,10 @@ int main(void) {
     char ground_truth[BUFF_SIZE]    = {0};  // used to verify
     char producer_buffer[BUFF_SIZE] = {0};  // used by the parent
     pid_t pid, w;
+    struct timespec sleeptime;
+
+    sleeptime.tv_sec = 0;
+    sleeptime.tv_nsec = TEN_MILLION;
 
     // init the ground truth and parent buffer
     for (int i = 0; i < BUFF_SIZE; ++i) {
@@ -39,7 +43,7 @@ int main(void) {
     const char *const p_msq_key = "/OS_MSG";
     const char *const p_shm_key = "/OS_SHM";
     const char *const p_sem_full_key = "/OS_SEM_FULL";
-    const char *const p_sem_empty_key = "/OS_SEM_EMPTY"
+    const char *const p_sem_empty_key = "/OS_SEM_EMPTY";
 
     /*
     * MESSAGE QUEUE SECTION
@@ -132,63 +136,65 @@ int main(void) {
     /*
     * PIPE SECTION
     **/
-    // int pfd[2];                             /* Pipe file descriptors */
-    // char buf[DATA_SIZE];
-    // ssize_t numRead;
-    // char consumer_pipe[BUFF_SIZE] = {0};
+    int pfd[2];                             /* Pipe file descriptors */
+    char buf[DATA_SIZE];
+    ssize_t numRead;
+    char consumer_pipe[BUFF_SIZE] = {0};
 
 
-    // if (pipe(pfd) == -1) return -1; //create the pipe
+    if (pipe(pfd) == -1) return -1; //create the pipe
 
-    // switch (pid = fork()) {
-    //     case -1:
-    //         return -1;
+    switch (pid = fork()) {
+        case -1:
+            return -1;
 
-    //     case 0:             /* Child  - reads from pipe */
-    //         if (close(pfd[1]) == -1)            /* Write end is unused */
-    //             return -1;
+        case 0:             /* Child  - reads from pipe */
+            if (close(pfd[1]) == -1)            /* Write end is unused */
+                return -1;
 
-    //         for (int i=0;i<BUFF_SIZE;i+=DATA_SIZE) {              /* Read data from pipe, echo on stdout */
-    //             //printf("\nREAD: %d", i);
-    //             numRead = read(pfd[0], buf, DATA_SIZE);
-    //             if (numRead == -1)
-    //                 return -1;
-    //             if (numRead == 0)
-    //                 break;                      /* End-of-file */
-    //             memcpy(consumer_pipe + i, buf, DATA_SIZE);
-    //         }
+            for (int i=0;i<BUFF_SIZE;i+=DATA_SIZE) {              /* Read data from pipe, echo on stdout */
+                //printf("\nREAD: %d", i);
+                numRead = read(pfd[0], buf, DATA_SIZE);
+                if (numRead == -1)
+                    return -1;
+                if (numRead == 0)
+                    break;                      /* End-of-file */
+                memcpy(consumer_pipe + i, buf, DATA_SIZE);
+                nanosleep(&sleeptime, NULL);
+            }
 
-    //         if (close(pfd[0]) == -1)
-    //             return -1;
+            if (close(pfd[0]) == -1)
+                return -1;
 
-    //         for (int i=0; i<BUFF_SIZE; ++i) {
-    //             if (memcmp(&ground_truth[i], &consumer_pipe[i], sizeof(char)) != 0) {
-    //                 printf("\nBuffers DO NOT match... at index: %d\n\nInput: %c\nOutput: %c\n", i, ground_truth[i], consumer_pipe[i]);
-    //                 exit(1);
-    //             }
-    //         }
-    //         printf("\nBuffers DO match!\n");
+            for (int i=0; i<BUFF_SIZE; ++i) {
+                if (memcmp(&ground_truth[i], &consumer_pipe[i], sizeof(char)) != 0) {
+                    printf("\nBuffers DO NOT match... at index: %d\n\nInput: %c\nOutput: %c\n", i, ground_truth[i], consumer_pipe[i]);
+                    exit(1);
+                }
+            }
+            printf("\nBuffers DO match!\n");
 
-    //         _exit(EXIT_SUCCESS);
+            _exit(EXIT_SUCCESS);
 
-    //     default:            /* Parent - writes to pipe */
-    //         if (close(pfd[0]) == -1)            /* Read end is unused */
-    //             return -1;
+        default:            /* Parent - writes to pipe */
+            if (close(pfd[0]) == -1)            /* Read end is unused */
+                return -1;
 
-    //         for (int i=0; i<BUFF_SIZE; i+=DATA_SIZE) {
-    //             //printf("\nWRITE: %d", i);
-    //             if (write(pfd[1], producer_buffer + i, DATA_SIZE) != DATA_SIZE)
-    //                 return -1;
-    //             sleep(1);
-    //         }
+            for (int i=0; i<BUFF_SIZE; i+=DATA_SIZE) {
+                //printf("\nWRITE: %d", i);
+                if (write(pfd[1], producer_buffer + i, DATA_SIZE) != DATA_SIZE)
+                    return -1;
+                nanosleep(&sleeptime, NULL);
+                //sleep(1);
+            }
 
-    //         if (close(pfd[1]) == -1)            /* Child will see EOF */
-    //             return -1;
+            if (close(pfd[1]) == -1)            /* Child will see EOF */
+                return -1;
 
-    //         w = waitpid(pid, NULL, 0);
-    //         printf("\nDONE PIPE\n");
-    //         if (w == -1) { perror("waitpid"); exit(EXIT_FAILURE); } /* Wait for child to finish */
-    // }
+            w = waitpid(pid, NULL, 0);
+            printf("\nDONE PIPE\n");
+            if (w == -1) { perror("waitpid"); exit(EXIT_FAILURE); } /* Wait for child to finish */
+    }
 
     /*
     * SHARED MEMORY AND SEMAPHORE SECTION
@@ -196,13 +202,7 @@ int main(void) {
     char consumer_shm[BUFF_SIZE] = {0};
     int shm_fd;
     void *ptr;
-    sem_t *semlock;
-    //int num;
-
-    struct timespec sleeptime;
- 
-    sleeptime.tv_sec = 0;
-    sleeptime.tv_nsec = TEN_MILLION;
+    sem_t *semlock_full, *semlock_empty;
 
     /* create the shared memory segment */
     shm_fd = shm_open(p_shm_key, O_CREAT | O_RDWR, 0666);
@@ -222,7 +222,9 @@ int main(void) {
         return -1;
     }
 
-    semlock = sem_open(p_sem_key, O_CREAT, S_IRUSR | S_IWUSR, 1);
+    semlock_full = sem_open(p_sem_full_key, O_CREAT, S_IRUSR | S_IWUSR, 1);
+    semlock_empty = sem_open(p_sem_empty_key, O_CREAT, S_IRUSR | S_IWUSR, 1);
+    sem_wait(semlock_full);
 
     switch(pid = fork()) {
         case -1:
@@ -232,12 +234,12 @@ int main(void) {
             // loop through and read all shm 
             for (int i=0;i<BUFF_SIZE; i+=DATA_SIZE) {
                 //printf("\nSEM: %d", sem_getvalue(semlock, &num));
-                sem_wait(semlock);
+                sem_wait(semlock_full);
                 memcpy(consumer_shm + i, ptr, DATA_SIZE);
-                printf("\nREAD: %d",i);
-                sem_post(semlock);
-                //nanosleep(&sleeptime, NULL);
-                sleep(1);
+                //printf("\nREAD: %d",i);
+                sem_post(semlock_empty);
+                nanosleep(&sleeptime, NULL);
+                //sleep(1);
             }
 
             for (int i=0; i<BUFF_SIZE; ++i) {
@@ -248,21 +250,25 @@ int main(void) {
             }
             printf("\nBuffers DO match!\n");
 
-            if (shm_unlink(p_shm_key) != 0) {
-                perror("In shm_unlink()");
-                exit(1);
-            }
+
             /**
              * Semaphore Close: Close a named semaphore
              */
-            if (sem_close(semlock) < 0 ) {
+            if (sem_close(semlock_full) < 0 ) {
                 perror("sem_close");
             }
 
+            if (sem_close(semlock_empty) < 0 ) {
+                perror("sem_close");
+            }
             /**
              * Semaphore unlink: Remove a named semaphore  from the system.
              */
-            if (sem_unlink(p_sem_key) < 0 ) {
+            if (sem_unlink(p_sem_full_key) < 0 ) {
+                perror("sem_unlink");
+            }
+
+            if (sem_unlink(p_sem_empty_key) < 0 ) {
                 perror("sem_unlink");
             }
 
@@ -271,17 +277,45 @@ int main(void) {
         default:
             // loop through and write all shm
             for (int i=0; i<BUFF_SIZE; i+=DATA_SIZE) {
-                sem_wait(semlock);
+                sem_wait(semlock_empty);
                 memcpy(ptr, producer_buffer + i, DATA_SIZE);
-                printf("\nWRITE: %d", i);
-                sem_post(semlock);
-                //nanosleep(&sleeptime, NULL);
-                sleep(1);
+                //printf("\nWRITE: %d", i);
+                sem_post(semlock_full);
+                nanosleep(&sleeptime, NULL);
+                //sleep(1);
             }
 
             w = waitpid(pid, NULL, 0);
             printf("\nDONE SHM\n");
             if (w == -1) { perror("waitpid"); exit(EXIT_FAILURE); }/* Wait for child to finish */
+    
+            munmap(ptr, DATA_SIZE);
+
+            if (shm_unlink(p_shm_key) != 0) {
+                perror("In shm_unlink()");
+                exit(1);
+            }
+
+            /**
+             * Semaphore Close: Close a named semaphore
+             */
+            if (sem_close(semlock_full) < 0 ) {
+                perror("sem_close");
+            }
+
+            if (sem_close(semlock_empty) < 0 ) {
+                perror("sem_close");
+            }
+            /**
+             * Semaphore unlink: Remove a named semaphore  from the system.
+             */
+            if (sem_unlink(p_sem_full_key) < 0 ) {
+                perror("sem_unlink");
+            }
+
+            if (sem_unlink(p_sem_empty_key) < 0 ) {
+                perror("sem_unlink");
+            }
     }
 
 
